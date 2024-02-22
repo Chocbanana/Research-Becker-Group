4e8c454f29f2052caf2e8bece535d7c9ac96c63e
Chocbanana <bjonnala@alumni.caltech.edu>
2024-02-21 17:44:17 -0700

    Working plasma 2D runs with HDF5 data

--- src/lib/datasets/gen_plasma_1d.py
+++ src/lib/datasets/gen_plasma_1d.py
@@ -2,6 +2,7 @@ import os
 from collections.abc import Sequence, Callable
 
 import numpy as np
+import h5py
 import torch
 from torch.utils.data import Dataset
 from torch import nn
@@ -15,11 +16,12 @@ class GenPlasma1DDataset(Dataset):
         """Loads matrices into tensors that are in the form:
         - Individual folders
         - Files in the folder with number seq N from 0-anything
-        - Files prefix with given `mats`, have `_[N].csv` as suffix
+        - Files prefix with given `mats`, have `[N].hdf5` as suffix
 
         Args:
-            mat_dirs (Sequence[str]): The dirs in which the csv files are located. All are used across the dataset loading
-            mats (Sequence[str], optional): The first character in the matrix filenames to load. Defaults to ["f"].
+            mat_dirs (Sequence[str]): The dirs in which the hdf5 files are located. All are used across the dataset loading
+            mats (Sequence[str], optional): The dataset(s) to load. Defaults to ["f"].
+            return_imname (bool, optional): Whether to also return the path+filename of the matrix
             transform (Callable, optional): A transform fcn to be applied to an matrix after loading. Defaults to None.
         """
         self.mat_dirs = mat_dirs
@@ -29,8 +31,7 @@ class GenPlasma1DDataset(Dataset):
 
         self._num_folders = len(mat_dirs)
         # Count the number of occurences of one type of matrix in each folder
-        self._folder_lens = [len([f for f in
-                                    os.listdir(i) if f.startswith(f"{mats[0]}_")])
+        self._folder_lens = [len([f for f in os.listdir(i) if f.endswith("hdf5")])
                                     for i in mat_dirs]
         self._folder_idxs = np.cumsum(self._folder_lens) - 1
         self._len = sum(self._folder_lens)
@@ -43,14 +44,14 @@ class GenPlasma1DDataset(Dataset):
     def __getitem__(self, idx):
         folder_num = np.searchsorted(self._folder_idxs, idx)
         mat_num = idx if folder_num == 0 else idx - self._folder_idxs[folder_num - 1] - 1
-        mat_paths = [os.path.join(self.mat_dirs[folder_num], f"{m}_{mat_num}.csv") for m in self.mat_names]
+        mat_path = os.path.join(self.mat_dirs[folder_num], f"{mat_num}.hdf5")
 
-        mats = tuple(torch.Tensor(np.genfromtxt(m, delimiter=",", dtype=float))
-                     for m in mat_paths)
+        with h5py.File(mat_path, 'r') as file:
+            mats = tuple(torch.Tensor(file[m][()]) for m in self.mat_names)
         if self.transform is not None:
             mats = tuple(self.transform(m) for m in mats)
 
         if self.return_imname:
-            mats = mats + (f"{self.mat_dirs[folder_num]}/{self.mat_names[0]}_{mat_num}",)
+            mats = mats + (os.path.join(f"{self.mat_dirs[folder_num]}", f"{mat_num}.hdf5"), idx)
 
         return mats[0] if len(mats) == 1 else mats
